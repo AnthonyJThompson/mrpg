228de0183f48edd374455af84c63b930fe1bf8ce
AnthonyJThompson <tehcoolzor@gmail.com>
2017-11-15 23:04:53 -0600

    setup fight scene

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -9,15 +8,17 @@
 export default {
   name: 'app'
 }
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration)
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- monster -->
+    <div id="monster">
+      <div><img :src="monster.img" /></div>
+      <small>{{ monster.name }}</small>
+    </div>
+    <!-- health -->
+    <div id="health">
+      <div id="monster-health">
+        <div></div>
+      </div>
+    </div>
+    <!-- controls -->
+    <div id="controls"></div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  data () {
+    return {
+      msg: 'something',
+      monster: {
+        name: 'bear',
+        img: require('../assets/bear.png'),
+        health: 10
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#monster{
+  margin: 0 auto;
+  text-align: center;
+}
+#monster-health{
+  border: 5px solid;
+  width: 200px;
+  margin: 0 auto 10px auto;
+}
+</style>
